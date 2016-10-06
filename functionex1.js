8bf6560a4ec49f2e7d903d9d0e58e7aeee815169
DaQunda Magwood <dmagwood13@gmail.com>
2016-10-05 21:18:38 -0400

    completed exerise 1, and added excerise 2 to file

--- functionex1.js
+++ functionex1.js
@@ -24,6 +24,10 @@
 // Write a function called `sum` that takes two
 // parameters and returns the sum of those 2 numbers.
 
+var sum = function(x, y) {
+	var result =  x + y;
+	return result;
+}
 
 
 
@@ -31,14 +35,28 @@
 // Write a function named `avg` that takes 3 parameters
 // and returns the average of those 3 numbers.
 
-
+var avg = function(x,y,z) {
+	var value= x+y+z
+	var valuetwo = value/3
+	return valuetwo;
+}
 
 
 // Q3
 // Write a function called `getLength` that takes one
 // parameter (a string) and returns the length
 
+var getLength = function(x){
 
+var value = xyz;
+
+If (number){
+return "6";
+} else {
+return number.toString();
+}
+// I believe this right, but im not sure. I know that length
+// adds togetheir whats inside a string.
 
 
 // Q4
@@ -46,6 +64,18 @@
 // two parameters and returns `true` if the second
 // parameter is greater than the first. Otherwise
 // the function should return `false`.
+
+var greaterThan = function(x,y) {
+	if (x 10 < 0 ) {
+		return "false";
+	}
+	else if (y 10 > 0) {
+		return "true"
+}	else {
+	return "false"
+}
+}
+		
 
 
 
@@ -55,6 +85,12 @@
 // single parameter and returns a string that
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
+
+var hello =  "Hello, Name!"
+var greet = function(name){
+	return "Hello, Name!" + name;
+}
+
 
 
 
@@ -70,6 +106,21 @@
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 
+var madlib = function(food,drink,snack,sweets){
+	if (food){
+		return "The baby threw food at me";
+	}
+	if (drink){
+		return "Its been a long day, so i need a drink";
+	}
+	if (snack){
+		return "cheddar popcorn is my favorite snack";
+	}
+	if (sweets){
+		return "chocolate is first in my sweets book";
+	}
+
+}
 
 
 
